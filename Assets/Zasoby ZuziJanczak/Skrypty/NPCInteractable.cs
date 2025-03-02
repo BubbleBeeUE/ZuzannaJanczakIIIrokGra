79bf9d32add985c1c84342d8143966a1301532b9
Zuzia Janczak <lululoloser@gmail.com>
2025-03-03 00:34:32 +0100

    Update

--- Assets/Zasoby ZuziJanczak/Skrypty/NPCInteractable.cs
+++ Assets/Zasoby ZuziJanczak/Skrypty/NPCInteractable.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+
 
 public class NPCInteractable : MonoBehaviour
 {
 
-    [SerializeField] UnityEvent talking;
+    [SerializeField] 
 
 
 
